cf371a89e9b955a6d351b32052edd2c2f6010a5e
Valeriya Trofimova <chaton-2410@mail.ru>
2022-06-04 18:16:49 +0300

    Initial Commit

---
+++ searchMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  searchMusic
+//
+//  Created by Valeriya Trofimova on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
